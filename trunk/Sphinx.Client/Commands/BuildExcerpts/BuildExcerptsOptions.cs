a869998495dfba26edb13b0ca5a233a21909ea8e
theplacefordev <theplacefordev@d1fc0914-b506-11de-9b33-83dc4fd2bb11>
2010-07-19 21:24:02 +0000

    added support for all new features introduced in Sphinx 1.10-beta related with BuildExcerpts function: - added new command parameter SnippetsCountLimit (limit_passages option) - added new command parameter WordsCountLimit (limit_words option) - added all new boolean options (ForceAllWords, LoadFiles, AllowEmpty) to BuildExcerptsOptions enum - added new command parameter StartPassageId (start_passage_id option) - added new command parameter HtmlStripMode (html_strip_mode option) represented as enum

--- trunk/Sphinx.Client/Commands/BuildExcerpts/BuildExcerptsOptions.cs
+++ trunk/Sphinx.Client/Commands/BuildExcerpts/BuildExcerptsOptions.cs
@@ -48,7 +48,26 @@ namespace Sphinx.Client.Commands.BuildExcerpts
         /// <summary>
         /// If flag is set, sort the extracted passages in order of relevance (decreasing weight), or in order of appearance in the document (increasing position) otherwise
         /// </summary>
-        OrderByWeight = 16
+        OrderByWeight = 16,
 
+		/// <summary>
+		/// Added in Sphinx 1.10-beta. Whether to handle keywords in list as a query in <a href="http://sphinxsearch.com/docs/current.html#extended-syntax">extended syntax</a>, or as a bag of words (default behavior). For instance, in query mode ("one two" | "three four") will only highlight and include those occurrences "one two" or "three four" when the two words from each pair are adjacent to each other. In default mode, any single occurrence of "one", "two", "three", or "four" would be highlighted.
+		/// </summary>
+		QueryMode = 32,
+
+		/// <summary>
+		/// Added in version 1.10-beta. Ignores the snippet length limit until it includes all the keywords.
+		/// </summary>
+		ForceAllWords = 64,
+
+		/// <summary>
+		/// Added in version 1.10-beta. Whether to handle documents in list as data to extract snippets from (default behavior), or to treat it as file names, and load data from specified files on the server side. 
+		/// </summary>
+		LoadFiles = 128,
+
+		/// <summary>
+		/// Added in version 1.10-beta. Allows empty string to be returned as highlighting result when a snippet could not be generated (no keywords match, or no passages fit the limit). By default, the beginning of original text would be returned instead of an empty string.
+		/// </summary>
+		AllowEmpty = 256
     }
 }
