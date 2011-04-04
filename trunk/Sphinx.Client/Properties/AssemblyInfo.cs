96aeb1043744798d3bd7d03632d3790a5ff2a55a
theplacefordev <theplacefordev@d1fc0914-b506-11de-9b33-83dc4fd2bb11>
2011-04-04 18:17:04 +0000

    Change mail assembly version to 0.4.0

--- trunk/Sphinx.Client/Properties/AssemblyInfo.cs
+++ trunk/Sphinx.Client/Properties/AssemblyInfo.cs
@@ -56,7 +56,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.3.9.0")]
-[assembly: AssemblyFileVersion("0.3.9.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Sphinx.Client.UnitTests")]
