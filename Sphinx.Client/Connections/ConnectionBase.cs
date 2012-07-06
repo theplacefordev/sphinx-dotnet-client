55ff765fb6559f51fac6f182a869b7f42111ab2e
theplacefordev <theplacefordev@d1fc0914-b506-11de-9b33-83dc4fd2bb11>
2012-07-06 22:52:27 +0000

    Persistent connection should not throw network IO exception, if it was manually opened by Open() method

--- Sphinx.Client/Connections/ConnectionBase.cs
+++ Sphinx.Client/Connections/ConnectionBase.cs
@@ -27,7 +27,7 @@ using Sphinx.Client.Network;
 
 namespace Sphinx.Client.Connections
 {
-    public abstract class ConnectionBase: IDisposable
+	public abstract class ConnectionBase: IDisposable
 	{
 		#region Fields
 		private string _host;
@@ -37,49 +37,49 @@ namespace Sphinx.Client.Connections
 
 		#region Constructors
 		/// <summary>
-        /// Initializes a new instance of the class. <see cref="Host"/> property must be specified before invoking <see cref="Open()"/> method.
-        /// </summary>
-        protected ConnectionBase()
-        {
-        }
+		/// Initializes a new instance of the class. <see cref="Host"/> property must be specified before invoking <see cref="Open()"/> method.
+		/// </summary>
+		protected ConnectionBase()
+		{
+		}
 
-        /// <summary>
-        /// Initializes a new instance of the class and binds it to the specified host. Default port value remains.
-        /// </summary>
-        /// <param name="host">Remote Sphinx server host address to which you intend to connect. Default port number value will be used to establish connection.</param>
-        protected ConnectionBase(string host): this()
-        {
-            Host = host;
-        }
+		/// <summary>
+		/// Initializes a new instance of the class and binds it to the specified host. Default port value remains.
+		/// </summary>
+		/// <param name="host">Remote Sphinx server host address to which you intend to connect. Default port number value will be used to establish connection.</param>
+		protected ConnectionBase(string host): this()
+		{
+			Host = host;
+		}
 
-        /// <summary>
-        /// Initializes a new instance of the class and binds it to the specified port on the specified host.
-        /// </summary>
-        /// <param name="host">Remote Sphinx server host address to which you intend to connect.</param>
-        /// <param name="port">The port number of the Sphinx server host to which you intend to connect.</param>
-        protected ConnectionBase(string host, int port): this(host)
-        {
-            Port = port;
-        }
+		/// <summary>
+		/// Initializes a new instance of the class and binds it to the specified port on the specified host.
+		/// </summary>
+		/// <param name="host">Remote Sphinx server host address to which you intend to connect.</param>
+		/// <param name="port">The port number of the Sphinx server host to which you intend to connect.</param>
+		protected ConnectionBase(string host, int port): this(host)
+		{
+			Port = port;
+		}
 
-        /// <summary>
-        /// Initializes a new instance of the class and binds it to the specified local endpoint.
-        /// </summary>
-        /// <param name="address">The <see cref="IPEndPoint"/> of remote Sphinx server containing host address and port to which you intend to connect.</param>
-        protected ConnectionBase(IPEndPoint address)
-        {
-            ArgumentAssert.IsNotNull(address, "address");
-            Host = address.Address.ToString();
-            Port = address.Port;
-        }
+		/// <summary>
+		/// Initializes a new instance of the class and binds it to the specified local endpoint.
+		/// </summary>
+		/// <param name="address">The <see cref="IPEndPoint"/> of remote Sphinx server containing host address and port to which you intend to connect.</param>
+		protected ConnectionBase(IPEndPoint address)
+		{
+			ArgumentAssert.IsNotNull(address, "address");
+			Host = address.Address.ToString();
+			Port = address.Port;
+		}
 
 		~ConnectionBase()
 		{
 			Dispose(false);
 		}
-        #endregion
+		#endregion
 
-        #region Properties
+		#region Properties
 
 		/// <summary>
 		/// Sphinx server host address.
@@ -107,87 +107,87 @@ namespace Sphinx.Client.Connections
 			}
 		}
 
-        #region Abstract
-        /// <summary>
-        /// Gets or sets the socket connection timeout. Specified in milliseconds.
-        /// </summary>
-        public abstract int ConnectionTimeout { get; set; }
+		#region Abstract
+		/// <summary>
+		/// Gets or sets the socket connection timeout. Specified in milliseconds.
+		/// </summary>
+		public abstract int ConnectionTimeout { get; set; }
 
-        /// <summary>
-        /// Gets a value indicating whether the underlying socket is connected to a remote host.
-        /// </summary>
-        public abstract bool IsConnected { get; }
+		/// <summary>
+		/// Gets a value indicating whether the underlying socket is connected to a remote host.
+		/// </summary>
+		public abstract bool IsConnected { get; }
 
 		/// <summary>
 		/// Character encoding used to encode and decode strings
 		/// </summary>
 		public abstract Encoding Encoding { get; set; }
-        
+		
 		/// <summary>
-        /// Returns network client socket object
-        /// </summary>
-        protected abstract ISocketAdapter Socket { get; set; }
+		/// Returns network client socket object
+		/// </summary>
+		protected abstract ISocketAdapter Socket { get; set; }
 
-        /// <summary>
-        /// Get underlying network data stream object
-        /// </summary>
+		/// <summary>
+		/// Get underlying network data stream object
+		/// </summary>
 		protected abstract IStreamAdapter DataStream { get; }
 
-        /// <summary>
-        /// Returns <see cref="BinaryFormatterFactory"/> object to create binary formmater to (de)serialize data
-        /// </summary>
-        internal protected abstract IBinaryFormatterFactory FormatterFactory { get; }
+		/// <summary>
+		/// Returns <see cref="BinaryFormatterFactory"/> object to create binary formmater to (de)serialize data
+		/// </summary>
+		internal protected abstract IBinaryFormatterFactory FormatterFactory { get; set; }
 
-        #endregion
+		#endregion
 
-        #endregion
+		#endregion
 
-        #region Methods
+		#region Methods
 
-        #region Implemented
+		#region Implemented
 
-        /// <summary>
-        /// Disposes this ñonnection and closes underlying socket.
-        /// </summary>
-        /// <filterpriority>2</filterpriority>
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+		/// <summary>
+		/// Disposes this ñonnection and closes underlying socket.
+		/// </summary>
+		/// <filterpriority>2</filterpriority>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
 
-        /// <summary>
-        /// Internal overloaded virtual version of Dispose method. Used to allow derived classes release allocated resources.
-        /// </summary>
-        /// <param name="disposing"></param>
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                Close();
-            }
-        }
+		/// <summary>
+		/// Internal overloaded virtual version of Dispose method. Used to allow derived classes release allocated resources.
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				Close();
+			}
+		}
 
-        #endregion
+		#endregion
 
-        #region Abstract
-        /// <summary>
-        /// Opens new connection to Sphinx server.
-        /// </summary>
-        public abstract void Open();
+		#region Abstract
+		/// <summary>
+		/// Opens new connection to Sphinx server.
+		/// </summary>
+		public abstract void Open();
 
-        /// <summary>
-        /// Closes opened connection to Sphinx server.
-        /// </summary>
-        public abstract void Close();
+		/// <summary>
+		/// Closes opened connection to Sphinx server.
+		/// </summary>
+		public abstract void Close();
 
-        /// <summary>
-        /// Performs specified command against this connection. Inetrnal method, called from command.
-        /// </summary>
-        /// <param name="command"></param>
-        internal abstract void PerformCommand(CommandBase command);
-        #endregion
+		/// <summary>
+		/// Performs specified command against this connection. Inetrnal method, called from command.
+		/// </summary>
+		/// <param name="command"></param>
+		internal abstract void PerformCommand(CommandBase command);
+		#endregion
 
-        #endregion
-    }
+		#endregion
+	}
 }
