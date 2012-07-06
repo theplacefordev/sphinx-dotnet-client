878b746d72091bece4795cbb4edd8aeb2e31a9db
theplacefordev <theplacefordev@d1fc0914-b506-11de-9b33-83dc4fd2bb11>
2012-07-06 22:52:27 +0000

    Persistent connection should not throw network IO exception, if it was manually opened by Open() method

--- trunk/Sphinx.Client/Connections/PersistentTcpConnection.cs
+++ trunk/Sphinx.Client/Connections/PersistentTcpConnection.cs
@@ -23,73 +23,83 @@ using Sphinx.Client.IO;
 
 namespace Sphinx.Client.Connections
 {
-    /// <summary>
-    /// Persistent connections allow to use single network connection to execute multiple commands that would otherwise require create new network connection for each command.
-    /// </summary>
-    public class PersistentTcpConnection : TcpConnection
-    {
-        #region Constants
-        private const short PERSIST_COMMAND_VERSION = 0;
-        private const int PERSIST_COMMAND_BODY = 1;
+	/// <summary>
+	/// Persistent connections allow to use single network connection to execute multiple commands that would otherwise require create new network connection for each command.
+	/// </summary>
+	public class PersistentTcpConnection : TcpConnection
+	{
+		#region Constants
+		private const short PERSIST_COMMAND_VERSION = 0;
+		private const int PERSIST_COMMAND_BODY = 1;
 
-        private static CommandInfo _persistCommandInfo = new CommandInfo(ServerCommand.Persist, PERSIST_COMMAND_VERSION);
-        #endregion
+		private static CommandInfo _persistCommandInfo = new CommandInfo(ServerCommand.Persist, PERSIST_COMMAND_VERSION);
+		#endregion
 
-        #region Constructors
-        public PersistentTcpConnection()
-        {
-        }
+		#region Constructors
+		public PersistentTcpConnection()
+		{
+		}
 
 		public PersistentTcpConnection(string host): base(host)
-        {
-        }
+		{
+		}
 
-        public PersistentTcpConnection(string host, int port): base(host, port)
-        {
-        }
+		public PersistentTcpConnection(string host, int port): base(host, port)
+		{
+		}
 
-        public PersistentTcpConnection(IPEndPoint address): base(address)
-        {
-        }
+		public PersistentTcpConnection(IPEndPoint address): base(address)
+		{
+		}
 
-	    #endregion
+		#endregion
 
-        #region Methods
+		#region Methods
+
         /// <summary>
-        /// Send request to Sphinx server using underlying data stream and process server response. 
-        /// Connection behaviour is changed - underlying network connection will not closed until <see cref="PersistentTcpConnection.Close()"/> method is called or object is disposed.
+        /// Open persistent connection to Sphinx server
         /// </summary>
-        /// <param name="command">Command to execute</param>
-        internal override void PerformCommand(CommandBase command)
-        {
-            ArgumentAssert.IsNotNull(command, "command");
-            if (!IsConnected)
-            {
-                Open();
-                SendHandshake();
-            }
-            command.Serialize(DataStream);
+	    public override void Open()
+	    {
+	        base.Open();
+            SendHandshake();
+	    }
+
+	    /// <summary>
+		/// Send request to Sphinx server using underlying data stream and process server response. 
+		/// Connection behaviour is changed - underlying network connection will not closed until <see cref="PersistentTcpConnection.Close()"/> method is called or object is disposed.
+		/// </summary>
+		/// <param name="command">Command to execute</param>
+		internal override void PerformCommand(CommandBase command)
+		{
+			ArgumentAssert.IsNotNull(command, "command");
+			if (!IsConnected)
+			{
+				Open();
+			}
+			command.Serialize(DataStream);
+			DataStream.Flush();
+			command.Deserialize(DataStream);
+		}
+
+		/// <summary>
+		/// Sends client protocol version and checks protocol version supported by server. 
+		/// Overriden version sends additional information to make connection persistent.
+		/// </summary>
+		protected override void SendHandshake()
+		{
+			base.SendHandshake();
+			// send ad-hoc 'persistent connection' command
+			IBinaryWriter writer = FormatterFactory.CreateWriter(DataStream);
+			_persistCommandInfo.Serialize(writer);
+			
+			// command body length
+			writer.Write(sizeof(int));
+			// enable persistent connection boolean flag
+			writer.Write(PERSIST_COMMAND_BODY);
             DataStream.Flush();
-            command.Deserialize(DataStream);
         }
-
-        /// <summary>
-        /// Sends client protocol version and checks protocol version supported by server. 
-        /// Overriden version sends additional information to make connection persistent.
-        /// </summary>
-        protected override void SendHandshake()
-        {
-            base.SendHandshake();
-            // send 'persistent connection' command
-            IBinaryWriter writer = FormatterFactory.CreateWriter(DataStream);
-            _persistCommandInfo.Serialize(writer);
-            
-            // command body length
-            writer.Write(sizeof(int));
-            // enable persistent connection boolean flag
-            writer.Write(PERSIST_COMMAND_BODY);
-        }
-        
-        #endregion
-    }
+		
+		#endregion
+	}
 }
